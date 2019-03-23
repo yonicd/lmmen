04df8ed1cd9a05400f94aec0f1e54b3f8ccf8a7e
Ben Bolker <bbolker@gmail.com>
2019-03-23 18:20:03 -0400

    revamping guts

--- R/cv.glmmlasso.R
+++ R/cv.glmmlasso.R
@@ -1,10 +1,11 @@
 #' @title Cross Validation for glmmLasso package
 #' @description Cross Validation for glmmLasso package as shown in example xxx
 #' @param dat data.frame, containing y,X,Z and subject variables
-#' @param form.fixed formaula, fixed param formula, Default: NULL
-#' @param form.rnd list, named list containing random effect formula, Default: NULL
-#' @param lambda numeric, vector containing lasso penalty levels, Default: seq(500, 0, by = -5)
+#' @param form.fixed formula, fixed param formula, Default: NULL
+#' @param form.rnd list, named list containing random effect formula
+#' @param lambda numeric, vector containing lasso penalty levels
 #' @param family family, family function that defines the distribution link of the glmm, Default: gaussian(link = "identity")
+#' @param progress logical
 #' @return list of a fitted glmmLasso object and the cv BIC path
 #' @examples
 #' \dontrun{cv.glmmLasso(initialize_example(seed=1))}
@@ -21,58 +22,70 @@ cv.glmmLasso=function(dat,
                       form.fixed=NULL,
                       form.rnd=NULL,
                       lambda=seq(500,0,by=-5),
-                      family=stats::gaussian(link = "identity")
-                      ){
+                      family=stats::gaussian(link = "identity"),
+                      progress = TRUE
+                      ) {
+
+    stopifnot(inherits(dat, "data.frame"))
+    d.size <- length(unique(dat$subject))*(sum(grepl('^Z',names(dat)))+1)+
+        (sum(grepl('^X',names(dat)))+1)
   
-  if(inherits(dat,'matrix')) dat <- as.data.frame(dat)
-    
-  d.size=(max(as.numeric(row.names(dat)))*(sum(grepl('^Z',names(dat)))+1))+(sum(grepl('^X',names(dat)))+1)
-  
-  dat<-data.frame(subject=as.factor(row.names(dat)),dat,check.names = FALSE,row.names = NULL)
-  
-  if(is.null(form.fixed)) form.fixed<-sprintf('y~%s',paste(grep('^X',names(dat),value = TRUE),collapse = '+'))
-  if(is.null(form.rnd)) form.rnd<-eval(parse(text=sprintf('form.rnd<-list(subject=~1+%s)',paste(grep('^Z',names(dat),value = TRUE),collapse = '+'))))
-  
-  BIC_vec<-rep(Inf,length(lambda))
-  
-  # specify starting values for the very first fit; pay attention that Delta.start has suitable length! 
-  
-  Delta.start.base<-Delta.start<-as.matrix(t(rep(0,d.size)))
-  Q.start.base<-Q.start<-0.1  
-  
-  for(j in 1:length(lambda))
-  {
-    suppressMessages({
-      suppressWarnings({
-        fn <- try(glmmLasso::glmmLasso(fix = stats::as.formula(form.fixed),
-                                       rnd = form.rnd,
-                                       data = dat,lambda = lambda[j],
-                                       switch.NR = FALSE,final.re=TRUE,
-                                       control = list(start=Delta.start[j,],q.start=Q.start[j]))      
-        )
-      })      
-    })
-    
-    if(class(fn)!="try-error")
-    {  
-      BIC_vec[j]<-fn$bic
-      Delta.start<-rbind(Delta.start,fn$Deltamatrix[fn$conv.step,])
-      Q.start<-c(Q.start,fn$Q_long[[fn$conv.step+1]])
-    }else{
-      Delta.start<-rbind(Delta.start,Delta.start.base)
-      Q.start<-c(Q.start,Q.start.base)
+    if (is.null(form.fixed)) {
+        form.fixed<- reformulate(grep("^X",names(dat), value=TRUE),
+                                 response="y")
     }
-  }
-  
-    opt<-which.min(BIC_vec)
-  
-    suppressWarnings({
-    final <- glmmLasso::glmmLasso(fix = as.formula(form.fixed), rnd = form.rnd,
-                                  data = dat, lambda=lambda[opt],switch.NR=FALSE,final.re=TRUE,
-                                  control = list(start=Delta.start[opt,],q_start=Q.start.base))
+    if(is.null(form.rnd)) {
+        ## form.rnd <- reformulate(grep("^Z",names(dat), value=TRUE),
+        ## response="subject")
+        form.rnd <- setNames(list(reformulate(grep("^Z",names(dat), value=TRUE))),
+                             "subject")
+    }
     
-    final
-  })
-  
-  list(fit.opt=final,BIC_path=BIC_vec)
+    if (progress) pb <- txtProgressBar(style=3, max=length(lambda))
+
+    get.lastpars <- function(f) {
+        list(start=f$Deltamatrix[f$conv.step,],
+             q.start=f$Q_long[[f$conv.step]])
+    }
+             
+    fit.list <- list()
+    fit.list[[1]] <-
+        try(glmmLasso::glmmLasso(fix = form.fixed,
+                                 rnd = form.rnd,
+                                 data = dat,
+                                 lambda = lambda[1],
+                                 switch.NR = FALSE,
+                                 final.re=FALSE),
+            silent=TRUE)
+    if (inherits(fit.list[[1]], "try-error")) {
+        stop("first lambda value failed")
+    }        
+    if (progress) setTxtProgressBar(pb, 1)
+    next.start <- get.lastpars(fit.list[[1]])
+        
+    for (j in seq_along(lambda)[-1]) {
+        if (progress) setTxtProgressBar(pb, j)        
+        fn <- suppressMessages(suppressWarnings(
+            try(glmmLasso::glmmLasso(fix = stats::as.formula(form.fixed),
+                                     rnd = form.rnd,
+                                     data = dat,
+                                     lambda = lambda[j],
+                                     switch.NR = FALSE,
+                                     final.re=FALSE,
+                                     control =
+                                         glmmLassControl(start=next.start$start,
+                                                         q.start=next.start$q.start)),
+                                     silent=TRUE)))
+        if (!inherits(fn,"try-error")) {
+            next.start <- get.lastpars(fn)
+            fit.list[[j]] <- fn
+        }
+    }
+
+    BIC_vec <- vapply(fit.list,
+                      function(x) if (is.null(x)) NA_real_ else x[["bic"]],
+                      numeric(1))
+    
+    fit.opt <- fit.list[[which.min(BIC_vec)]]
+    list(fit.opt=fit.opt, BIC_path=BIC_vec)
 }  
--- R/initialize_example.R
+++ R/initialize_example.R
@@ -23,40 +23,37 @@
 #' @export 
 #' @importFrom mvtnorm rmvnorm
 #' @importFrom stats runif
-initialize_example=function(n.i = 5,n = 30,q=4,total.beta=9,true.beta=c(1,1,1),seed=NULL){
+initialize_example <- function(n.i = 5,n = 30,q=4,total.beta=9,
+                               true.beta=c(1,1,1),seed=NULL){
   
   if(!is.null(seed)) set.seed(seed)
   
-  y = NULL 
-  X = NULL 
-  Z = NULL 
+  y <- X <- Z <-  NULL 
   subject = kronecker(1:n, rep(1,n.i))
-  
-  true.beta = c(true.beta, rep(0,c(total.beta-length(true.beta))))
-  
-  if(q<10){
-    true.D = matrix(c(9,4.8,0.6,0, 4.8,4,1,0, .6,1,1,0, 0,0,0,0), nrow=q, ncol=q)
-  }else{
-    true.D = matrix(c(9,4.8,0.6,0, 4.8,4,1,0, .6,1,1,0, 0,0,0,0), nrow=4, ncol=4)
+    true.beta = c(true.beta, rep(0,c(total.beta-length(true.beta))))
+  bvec <- c(9,4.8,0.6,0, 4.8,4,1,0, .6,1,1,0, 0,0,0,0)
+  if (q<10) {
+    true.D = matrix(bvec, nrow=q, ncol=q)
+  } else {
+    true.D = matrix(bvec, nrow=4, ncol=4)
     true.D=rbind(cbind(true.D,matrix(0,nrow=4,ncol=q-4)),matrix(0,nrow=q-4,ncol=q))
   }
-  for (i in 1:n)  
-  { 
-    Xi = cbind(matrix(stats::runif(length(true.beta)*n.i,-2,2), nrow=n.i)) 
-    Zi=switch((q<10)+1,{
-      cbind(1,Xi)
-    },
-    {
-      cbind(1,matrix(stats::runif((nrow(true.D)-1)*n.i,-2,2), nrow=n.i))}
-    )
-    X = rbind(X, Xi) 
-    Z = rbind(Z, Zi[,-1]) 
-    S = Zi%*%true.D%*%t(Zi) + diag(n.i) 
-    y = rbind(y, t(mvtnorm::rmvnorm(1, Xi%*%true.beta, S)) ) 
+  for (i in 1:n) { 
+      Xi = cbind(matrix(stats::runif(length(true.beta)*n.i,-2,2), nrow=n.i)) 
+      Zi=switch((q<10)+1,{
+          cbind(1,Xi)
+      },
+      {
+          cbind(1,matrix(stats::runif((nrow(true.D)-1)*n.i,-2,2), nrow=n.i))}
+      )
+      X = rbind(X, Xi) 
+      Z = rbind(Z, Zi[,-1]) 
+      S = Zi%*%true.D%*%t(Zi) + diag(n.i) 
+      y = rbind(y, t(mvtnorm::rmvnorm(1, Xi%*%true.beta, S)) ) 
   }
 
-  m=cbind(y,X,Z)
-  colnames(m)=c('y',sprintf('X%s',1:ncol(X)),sprintf('Z%s',1:ncol(Z)))
-  row.names(m)=subject
-  m
+    m=cbind(y,X,Z)
+    colnames(m)=c('y',sprintf('X%s',1:ncol(X)),sprintf('Z%s',1:ncol(Z)))
+    m <- data.frame(subject=factor(subject), m)
+    return(m)
 }
